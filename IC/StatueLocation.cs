b057a908878cc6e35bb60fa17a23576299f27df2
nerthul11 <=>
2024-01-24 10:19:02 -0300

    Emptied Location def to try new methods

--- IC/StatueLocation.cs
+++ IC/StatueLocation.cs
@@ -1,7 +1,13 @@
-using System;
+using HutongGames.PlayMaker;
 using ItemChanger;
+using ItemChanger.Extensions;
+using ItemChanger.FsmStateActions;
 using ItemChanger.Locations;
-using Steamworks;
+using ItemChanger.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace HallOfGodsRandomizer.IC 
 {
@@ -9,101 +15,114 @@ namespace HallOfGodsRandomizer.IC
     /// Each location should add an item when completing a tier for the first time.
     /// The method for setting statueState completion boolean values should be overriden.
     /// </summary>
-    public enum Tier
+    public class VoidIdolAggregatorModule : Module
     {
-        Unlock,
-        Attuned,
-        Ascended,
-        Radiant
+        private Dictionary<int, List<VoidIdolLocation>> statuePlacements = new();
+        private ParametricFsmEditBuilder<int>? statueEditBuilder;
+
+        public override void Initialize()
+        {
+            statueEditBuilder = new(GenerateStatueEdit);
+            for (int i = 0; i < 3; i++)
+            {
+                string goFullPath = $"/GG_Statue_Knight/Base/Statue/Knight_v0{i + 1}/Interact";
+                Events.AddFsmEdit(SceneNames.GG_Workshop, new(goFullPath, "Conversation Control"), statueEditBuilder.GetOrAddEdit(i));
+            }
+        }
+
+        public override void Unload()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string goFullPath = $"/GG_Statue_Knight/Base/Statue/Knight_v0{i + 1}/Interact";
+                Events.RemoveFsmEdit(SceneNames.GG_Workshop, new(goFullPath, "Conversation Control"), statueEditBuilder![i]);
+            }
+        }
+
+        private bool AreAllStatuePlacementsCleared(int statueTier)
+        {
+            if (statuePlacements.TryGetValue(statueTier, out List<VoidIdolLocation> locs))
+            {
+                return locs.All(x => x.Placement.AllObtained());
+            }
+            // if there's no placement... well of course the entire statue is cleared, there's no items here.
+            return true;
+        }
+
+        private void ChainGiveAllPlacementsAsync(int statueTier, Transform t, Action callback)
+        {
+            if (!statuePlacements.TryGetValue(statueTier, out List<VoidIdolLocation> locs))
+            {
+                callback?.Invoke();
+            }
+
+            Action aggregated = () => callback?.Invoke();
+            foreach (VoidIdolLocation loc in locs)
+            {
+                aggregated = ConcatGiveAll(aggregated, t, loc);
+            }
+            aggregated?.Invoke();
+
+            static Action ConcatGiveAll(Action aggregated, Transform t, VoidIdolLocation loc)
+            {
+                return () => loc.GiveAllAsync(t)(aggregated);
+            }
+        }
+
+        private Action<PlayMakerFSM> GenerateStatueEdit(int statueTier)
+        {
+            return (self) =>
+            {
+                FsmState journal = self.GetState("Journal");
+                journal.Actions = new FsmStateAction[]
+                {
+                    new DelegateBoolTest(() => AreAllStatuePlacementsCleared(statueTier), "FINISHED", null),
+                    new AsyncLambda((callback) => ChainGiveAllPlacementsAsync(statueTier, self.gameObject.transform, callback))
+                };
+            };
+        }
+
+        public void PlaceItemsAtStatue(int statueTier, VoidIdolLocation loc)
+        {
+            if (!statuePlacements.ContainsKey(statueTier))
+            {
+                statuePlacements[statueTier] = new List<VoidIdolLocation>();
+            }
+            if (!statuePlacements[statueTier].Contains(loc))
+            {
+                statuePlacements[statueTier].Add(loc);
+            }
+        }
+
+        public void RemoveItemsFromStatue(int statueTier, VoidIdolLocation loc)
+        {
+            if (statuePlacements.TryGetValue(statueTier, out List<VoidIdolLocation> locs))
+            {
+                locs.Remove(loc);
+            }
+        }
     }
+    
     
     public class StatueLocation : AutoLocation
     {
+        public enum Tier
+        {
+            Unlock,
+            Attuned,
+            Ascended,
+            Radiant
+        }
         public string statueStateName { get; set; }
         public Tier statueTier { get; set; }
         public string statueName { get; set; }
-        public bool unlock { get; private set; }
-        public bool attuned { get; private set; }
-        public bool ascended { get; private set; }
-        public bool radiant { get; private set; }
 
         protected override void OnUnload()
         {
-            On.BossStatue.SetPlaqueState -= BossStatue_SetPlaqueState;
-            On.BossStatue.UpdateDetails -= BossStatue_UpdateDetails;
         }
 
         protected override void OnLoad()
         {
-            On.BossStatue.SetPlaqueState += BossStatue_SetPlaqueState;
-            On.BossStatue.UpdateDetails += BossStatue_UpdateDetails;
-        }
-
-        private void BossStatue_UpdateDetails(On.BossStatue.orig_UpdateDetails orig, BossStatue self)
-        {
-            if (self.statueStatePD == statueStateName)
-            {
-                HallOfGodsRandomizer.Instance.Log($"BossStatue_UpdateDetails | {statueStateName}{statueTier}");
-
-                // Save current statue state depending on settings
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeStatueAccess == Menu.StatueAccessMode.Randomized)
-                {
-                    unlock = self.StatueState.isUnlocked;
-                }
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeTiers > Menu.TierLimitMode.Vanilla)
-                {
-                    attuned = self.StatueState.completedTier1;
-                }
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeTiers > Menu.TierLimitMode.ExcludeAscended)
-                {
-                    ascended = self.StatueState.completedTier2;
-                }
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeTiers > Menu.TierLimitMode.ExcludeRadiant)
-                {
-                    radiant = self.StatueState.completedTier3;
-                }
-            }
-            orig(self);
-            if (self.statueStatePD == statueStateName)
-            {
-                // Check the statue state matches with the stored variable, if there are mismatches, this is the critical event.
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeStatueAccess == Menu.StatueAccessMode.Randomized)
-                {
-                    HallOfGodsRandomizer.Instance.Log(unlock == self.StatueState.isUnlocked);
-                }
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeTiers > Menu.TierLimitMode.Vanilla)
-                {
-                    HallOfGodsRandomizer.Instance.Log(attuned == self.StatueState.completedTier1);
-                }
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeTiers > Menu.TierLimitMode.ExcludeAscended)
-                {
-                    HallOfGodsRandomizer.Instance.Log(ascended == self.StatueState.completedTier2);
-                }
-                if (HallOfGodsRandomizer.Instance.GS.MainSettings.RandomizeTiers > Menu.TierLimitMode.ExcludeRadiant)
-                {
-                    HallOfGodsRandomizer.Instance.Log(radiant == self.StatueState.completedTier3);
-                }
-            }   
-        }
-
-        private void BossStatue_SetPlaqueState(On.BossStatue.orig_SetPlaqueState orig, BossStatue self, BossStatue.Completion statueState, BossStatueTrophyPlaque plaque, string playerDataKey)
-        {
-            HallOfGodsRandomizer.Instance.Log($"BossStatue_SetPlaqueState | {statueStateName} {self.statueStatePD}");
-            if (self.statueStatePD == statueStateName)
-            {
-                HallOfGodsRandomizer.Instance.Log($"BossStatue_SetPlaqueState | {statueStateName}{statueTier}");
-                HallOfGodsRandomizer.Instance.Log($"self={self.statueStatePD} | statueState={statueState} | plaque={plaque} | pdk={playerDataKey}");
-                if (!Placement.AllObtained() && statueTier == Tier.Unlock)
-                {
-                    HeroController.instance.RelinquishControl();
-                    Placement.GiveAll(new()
-                    {
-                        FlingType = FlingType.DirectDeposit,
-                        MessageType = MessageType.Any
-                    }, HeroController.instance.RegainControl);
-                }
-            }
-            orig(self, statueState, plaque, playerDataKey);
         }
     }
 }
