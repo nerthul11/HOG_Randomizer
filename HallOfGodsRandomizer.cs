fe24aea0630ffaee31a04d488e4c0dd57d9464b2
nerthul11 <=>
2024-02-28 00:19:47 -0300

    Fixed Sheo's sprite.

--- HallOfGodsRandomizer.cs
+++ HallOfGodsRandomizer.cs
@@ -8,7 +8,7 @@ namespace HallOfGodsRandomizer
     public class HallOfGodsRandomizer : Mod, ILocalSettings<LocalSettings>, IGlobalSettings<GlobalSettings>
     {
         new public string GetName() => "HallOfGodsRandomizer";
-        public override string GetVersion() => "1.1.1.2";
+        public override string GetVersion() => "1.1.1.3";
 
         private static HallOfGodsRandomizer _instance;
         public HallOfGodsRandomizer() : base()
